e0ac682b4717fb3cccce70c0795b801b34b9a3ad
deselbi <deselbi@users.noreply.github.com>
2020-04-09 12:12:30 +0200

    step9 resubmited

--- step09/main.js
+++ step09/main.js
@@ -27,6 +27,33 @@ Vue.component("jumbotron", {
   }
 });
 
+Vue.component("modal", {
+  props: ["title", "body"],
+  template: `
+      <div class="modal is-active">
+          <div class="modal-background"></div>
+          <div class="modal-content">
+       
+          <slot>
+          </slot>
+          </div>
+          <button class="modal-close is-large" aria-label="close" @click="$emit('closeme')"> cls mod</button>
+        </div>
+
+    `,
+  methods: {
+    hideModal() {
+      this.isVisible = false;
+    }
+  },
+
+  data() {
+    return {
+      isVisible: false
+    };
+  }
+});
+
 new Vue({
   el: "#root",
   data: {}
