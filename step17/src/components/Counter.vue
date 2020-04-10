c7b71ea810953fbda569c60872f8a989fbbdda8b
deselbi <deselbi@users.noreply.github.com>
2020-04-11 00:44:28 +0200

    step17

---
+++ step17/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h2> count is  {{ count }}</h2>
+
+  <p>
+    <button @click="count++"> Increment</button>
+  </p>
+</div>
+
+</template>
+
+
+<script>
+
+  export default {
+
+    data () {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<style>
+
+
+</style>
