251a623584933be881d259c88923d68cb604b418
deselbi <deselbi@users.noreply.github.com>
2020-04-10 21:21:27 +0200

    step16

---
+++ step16/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+    <MessageComponent color="green"> Hello there</MessageComponent>
+
+    <MessageComponent :color="color"> Hello there again</MessageComponent>
+
+    <MessageComponent> Hello there again and again</MessageComponent>
+  </div>
+</template>
+
+<script>
+
+import MessageComponent from "./components/message";
+
+export default {
+  name: 'app',
+  components: { MessageComponent },
+  data () {
+    return {
+      color: "black"
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ step16/src/components/message.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="box">
+
+
+  <p>
+
+    this is defined in parent as slot
+    <slot></slot>
+  </p>
+
+  this link is defined as component property
+  <a href="#"> {{color}}</a>
+
+  and this link is defined as component state
+  <a href="#"> {{msg}}</a>
+
+</div>
+</template>
+
+<script>
+  export default {
+    props: {
+        color: {
+          default: "blue"
+        }
+    },
+    data () {
+      return {
+        msg: "bla bla msg"
+      }
+    }
+  }
+</script>
+
+<style>
+
+  .box {
+    background: yellow;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #001f3f;
+  ;
+  }
+
+</style>
