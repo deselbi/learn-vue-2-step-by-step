13190295b9bb9cc51ec25c763c4e4807aacf8eef
deselbi <deselbi@users.noreply.github.com>
2020-04-12 23:40:33 +0200

    Step19
    @onkeydown implemented in input

--- step18/public/js/app.js
+++ step18/public/js/app.js
@@ -1,12 +1,59 @@
 
+class Errors {
+
+    constructor(){
+        this.errors = [];
+    }
+
+    get(field){
+        if(this.errors[field]){
+            return this.errors[field][0];
+        }
+    }
+
+
+    record(errors) {
+        this.errors = errors;
+    }
+
+
+    clear(field) {
+        delete this.errors[field]
+    }
+
+
+};
 
 new Vue({
     el: "#root",
 
     data: {
-        skills: []
+        skills: [],
+
+        project: {
+            name: "",
+            description: ""
+        },
+        errors: new Errors()
+
     },
 
+    methods: {
+        onSubmit() {
+            axios.post('/projects', this.$data.project)
+                .then(this.onSuccess)
+                .catch(  error =>  {
+                    this.errors.record( error.response.data.errors) ;
+                    alert( error.response.data.message )
+                });
+
+        },
+
+        onSuccess(response) {
+            alert(response.data.message);
+
+        }
+    },
     mounted() {
 
         axios.get('/skills').then(
--- step18/resources/views/projects/create.blade.php
+++ step18/resources/views/projects/create.blade.php
@@ -8,28 +8,28 @@
 </head>
 
 <body>
-<div id="app" class="container">
+<div id="root" class="container">
     @include ('projects.list')
 
-    <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
+    <form method="POST" action="/projects" @submit.prevent="onSubmit">
         <div class="control">
             <label for="name" class="label">Project Name:</label>
 
-            <input type="text" id="name" name="name" class="input" v-model="form.name">
+            <input type="text" id="name" name="name" class="input" v-model="project.name" @keydown="errors.clear('name')">
 
-            <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+            <span class="help is-danger" v-text="errors.get('name')"></span>
         </div>
 
         <div class="control">
             <label for="description" class="label">Project Description:</label>
 
-            <input type="text" id="description" name="description" class="input" v-model="form.description">
+            <input type="text" id="description" name="description" class="input" v-model="project.description" @keydown="errors.clear('description')">
 
-            <span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
+            <span class="help is-danger" v-text="errors.get('description')"></span>
         </div>
 
         <div class="control">
-            <button class="button is-primary" :disabled="form.errors.any()">Create</button>
+            <button class="button is-primary" >Create</button>
         </div>
     </form>
 </div>
